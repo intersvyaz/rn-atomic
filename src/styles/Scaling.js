ded28044dc2966858d9e3144d8acb45aa8646f61
demothreen <demothreen@intersvyaz.net>
2019-04-17 09:28:03 +0500

    fix scaling style in landscape orientation

--- src/styles/Scaling.js
+++ src/styles/Scaling.js
@@ -1,13 +1,14 @@
 import { Dimensions } from 'react-native';
-
 const { width, height } = Dimensions.get('window');
 
 //Guideline sizes are based on standard ~5" screen mobile device
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = size => width / guidelineBaseWidth * size;
-const verticalScale = size => height / guidelineBaseHeight * size;
+const orientation = width < height ? width : height;
+
+const scale = size => orientation / guidelineBaseWidth * size;
+const verticalScale = size => orientation / guidelineBaseHeight * size;
 const moderateScale = (size, factor = 0.5) => size + ( scale(size) - size ) * factor;
 
 export {scale, verticalScale, moderateScale};
