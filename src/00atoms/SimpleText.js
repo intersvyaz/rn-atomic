d8ff4727149e03cdff5a7f5caded0a8e9a7e6c81
demothreen <demothreen@intersvyaz.net>
2019-05-07 15:35:04 +0500

    fix font scaling on button

--- src/00atoms/SimpleText.js
+++ src/00atoms/SimpleText.js
@@ -5,7 +5,7 @@ import {TextStyles} from '../styles/Base'
 export default class SimpleText extends React.Component {
   render(){
     return(
-      <Text style = {TextStyles.defaultText} {...this.props}>
+      <Text allowFontScaling={false} style = {TextStyles.defaultText} {...this.props}>
         {this.props.children}
       </Text>
     );
